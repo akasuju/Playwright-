a36235372437432f0fe83c909eeec07600d7d601
Sujan khatri <sujan.chettri1321@gmail.com>
2025-07-29 21:07:16 +0545

    Running first test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,14 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
+const {test,expect}=require('@playwright/test')
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("myfirsttest",async function({page}) {
+  
+  expect(12).toBe(12)
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+})
+test("secondtest", async function ({page}) {
+  expect(12).toBe(102)
+})
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+test("thirdtest", async function ({page}) {
+  expect(120).toBe(120)
+})
